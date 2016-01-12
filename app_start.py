3b73adfafd5122125d69f462902bfc5d80db979a
William <billreithmeyer@gmail.com>
2016-01-12 11:29:41 -0500

    test
    testing python code

--- app_start.py
+++ app_start.py
@@ -6,12 +6,30 @@ from hashlib import md5
 from datetime import datetime
 from flask import Flask, render_template
 
+appList = [
+    {
+        "name"  : "First app name",
+        "url"   : "/firstApp"
+    },
+    {
+        "name"  : "Second app name",
+        "url"   : "/secondApp"
+    },
+    {
+        "name"  : "Third app name",
+        "url"   : "/thirdApp"
+    },
+    {
+        "name"  : "Forth app name",
+        "url"   : "/fourthApp"
+    }
+]
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", appList)
 
 @app.route("/test")
 def test():
