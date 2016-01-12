3dbb7210570874793692ad51e96a21438842abb8
William <billreithmeyer@gmail.com>
2016-01-11 22:03:22 -0500

    test page

--- app_start.py
+++ app_start.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
