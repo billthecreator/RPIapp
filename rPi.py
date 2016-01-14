40b3bc49ac1e9e6815e128440351ff25543f1b68
William <billreithmeyer@gmail.com>
2016-01-14 18:36:47 -0500

    signup tests

--- rPi.py
+++ rPi.py
@@ -112,8 +112,8 @@ def index():
         return render_template('login.html')
     return render_template("index.html", appList=appList)
 
-@app.route("/signin")
-def signin():
+@app.route("/signup")
+def signup():
     return render_template("register.html")
 
 
